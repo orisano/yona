11a9f21b65b6a8e2ee6576893e3986e9bdc171cb
Nao Yonashiro <owan.orisano@gmail.com>
2017-05-27 15:58:03 +0900

    feat: add HttpServletFilter

---
+++ src/main/java/yona/filters/HttpServletFilter.java
@@ -0,0 +1,22 @@
+package yona.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public abstract class HttpServletFilter implements Filter {
+    @Override
+    public final void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        doFilter((HttpServletRequest)request, (HttpServletResponse)response, chain);
+    }
+
+    public abstract void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException;
+}
+
